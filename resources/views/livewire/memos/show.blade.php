08ca8350c229f029f8b7e6b447bb412a941cc0b2
beeracle-satoyuya <official@beracle.jp>
2025-08-31 14:31:55 +0900

    削除機能完成

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -11,6 +11,11 @@ $edit = function () {
      // 編集ページにリダイレクト
     return redirect()->route('memos.edit', $this->memo);
 };
+
+$destroy = function(){
+    $this->memo->delete();
+    return redirect()->route('memos.index');
+};
 ?>
 
 <div>
@@ -19,4 +24,5 @@ $edit = function () {
     <p>{!! nl2br(e($memo->body)) !!}</p>
 
     <button wire:click="edit">編集する</button>
+    <button wire:click="destroy" wire:confirm="本当に削除しますか？">削除する</button>
 </div>
